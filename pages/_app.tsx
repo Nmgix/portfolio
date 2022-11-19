04b1c1c3b8d6395d74a4bb0954ae6bc212af2284
Nmgix <frostdraky@gmail.com>
2022-11-19 04:35:31 +0300

    Temporary fix of bundling components with app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.scss";
+import "nmgix-components/src/index.scss";
 import type { AppProps } from "next/app";
 import { AppContainer } from "nmgix-components/src";
 import React from "react";
