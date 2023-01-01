8d6df5aac9fc226a2e284eabda6d59c2e338f0c1
Nmgix <frostdraky@gmail.com>
2023-01-01 06:54:45 +0300

    Small vercel predeploy fixes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withTM = require("next-transpile-modules")(["nmgix-components"]);
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = withTM({
   reactStrictMode: false,
   i18n: {
@@ -12,8 +10,6 @@ const nextConfig = withTM({
   publicRuntimeConfig: {
     HCAPTCHA_SITEKEY: process.env.HCAPTCHA_SITEKEY,
   },
-  assetPrefix: isProd ? "/portfolio/" : "/",
-  basePath: isProd ? "/portfolio" : "",
 });
 
 module.exports = nextConfig;
